aa5aa746a0865da8ad71dec825bba26386bdefb9
Szymon Dudziak <szymon.dudziak@rst.com.pl>
2016-09-27 20:28:40 +0200

    Fixes

--- test/dummy.spec.ts
+++ test/dummy.spec.ts
@@ -1,11 +1,11 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 describe('Dummy test', () => {
-    it('should true to be true', () => {
-        expect(true).to.be.equal(true);
-    });
+  it('should true to be true', () => {
+    expect(true).to.be.equal(true);
+  });
 
-    it('should false to be false', () => {
-        expect(true).to.be.equal(true);
-    });
+  it('should false to be false', () => {
+    expect(true).to.be.equal(true);
+  });
 });

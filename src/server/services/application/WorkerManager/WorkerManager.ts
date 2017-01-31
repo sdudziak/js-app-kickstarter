8f980c6259d2202f98808d9a5b5e22365bd24860
Szymon Dudziak <szymon.dudziak@rst.com.pl>
2017-01-31 21:35:17 +0100

    Working spawning for sub processes; still  scratch

--- src/server/services/application/WorkerManager/WorkerManager.ts
+++ src/server/services/application/WorkerManager/WorkerManager.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import { fork, ChildProcess } from 'child_process';
+import * as process from 'process';
 
 import { IWorkerManager } from './IWorkerManager';
 import { provideSingleton, inject } from '../../../ioc/ioc';
@@ -10,22 +12,40 @@ import { ILogger } from '../../logger/ILogger';
 export class WorkerManager implements IWorkerManager {
 
     private workersDir: string;
+    private workers: ChildProcess[];
 
     public constructor(@inject(TYPES.ILogger) private logger: ILogger) {
         this.workersDir = path.resolve(__dirname, '..', '..', '..', 'worker');
+        this.workers    = [];
     }
 
     public loadWorkers(): Promise<void> {
-        this.logger.log(`Loading workers from ${this.workersDir}`);
+        return this.getWorkersLocations()
+            .then((files: string[]) => files.forEach(this.forkSingleWorker.bind(this)));
+    }
+
+    private getWorkersLocations(): Promise<string[]> {
         return new Promise((resolve, reject) => {
             return fs.readdir(this.workersDir, (error: Error, files: string[]) => {
                 if (error) return reject(error);
-                files
+                resolve(files
                     .filter((file: string) => file.match(/\.js$/))
-                    .map((file: string) => path.resolve(file))
-                    .forEach((absolutePath: string) => this.logger.log(absolutePath));
-                resolve();
+                    .map((file: string) => path.resolve(this.workersDir, file))
+                );
             });
-        });
+        })
+    }
+
+    private forkSingleWorker(workerModulePath: string) {
+        const worker: ChildProcess = fork(workerModulePath);
+        this
+            .linkWorkerWithMainProcess(worker)
+            .workers.push(worker);
+    }
+
+    private linkWorkerWithMainProcess(worker: ChildProcess): this {
+        process.on('exit', () => worker.emit('exit'));
+        worker.on('exit', () => console.log('Finish my work!'));
+        return this;
     }
 }
--- src/server/worker/HeartbeatWorker.ts
+++ src/server/worker/HeartbeatWorker.ts
@@ -1,8 +1,17 @@
+import * as process from 'process';
+
 import { IWorker } from '../services/application/WorkerManager/IWorker';
 import { inject, provideSingleton } from '../ioc/ioc';
 import TYPES from '../constant/types';
 
+
 @provideSingleton(TYPES.IWorker)
 export class HeartbeatWorker implements IWorker {
 
+    public constructor() {
+        setInterval(() => console.log('I\'m alive!'), 1000);
+        process.on('exit', () => console.log('I quit!'));
+    }
 }
+
+exports = module.exports = new HeartbeatWorker();
